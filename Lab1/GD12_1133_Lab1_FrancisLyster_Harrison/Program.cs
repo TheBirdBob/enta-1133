040de74cb485fb92841168df8230c62850e0afcc
TheBirdBob <135292573+TheBirdBob@users.noreply.github.com>
2024-09-06 12:13:11 -0700

    Initial Creation of my folder

---
+++ Lab1/GD12_1133_Lab1_FrancisLyster_Harrison/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_FrancisLyster_Harrison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Harrison Francis-Lyster ENTA 1133");
+        }
+    }
+}
